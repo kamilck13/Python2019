9e6bb60786dde8c54ef7afb270e9104e6a648cc2
kamilck13 <kamil.ck13@gmail.com>
2020-01-05 16:59:20 +0100

    Zestaw 11

---
+++ Zadanie 11/zad11_1.py
@@ -0,0 +1,45 @@
+#11.1
+import random
+import math
+
+def losowe_liczby(N):
+    L = []
+    for i in range(0, int((random.random())*1000)):
+        L.append(random.randint(0, N-1))
+    return L
+
+def prawie_posortowana(N):
+    L = []
+    for i in range(0, int((random.random())*1000)):
+        a = i % N
+        if a % 2 == 0:
+            L.append(a+1)
+        else:
+            L.append(a-1)
+    return L
+
+def prawie_posortowana_odwrotnie(N):
+    L = prawie_posortowana(N)
+    L.reverse()
+    return L
+
+def losowe_gauss(N, mu, sigma):
+    L = []
+    for x in range(0, N):
+        L.append(random.gauss(mu, sigma))
+    return L
+
+def losowe_powtarzajace_sie(N):
+    L = []
+    X = []
+    for i in range(0, int(math.sqrt(N))):
+        X.append(random.randint(0, int((random.random())*1000)))
+    for i in range(0, N):
+        L.append(random.choice(X))
+    return L
+
+#print(losowe_liczby(9))
+#print(prawie_posortowana(9))
+#print(prawie_posortowana_odwrotnie(9))
+#print(losowe_gauss(1, 9, 1))
+#print(losowe_powtarzajace_sie(9))
---
+++ Zadanie 11/zad11_3.py
@@ -0,0 +1,20 @@
+#11.3
+def cmp(number1, number2):
+    if number1 > number2:
+        return 1
+    elif number1 < number2:
+        return -1
+    return 0
+
+def selectsort(L, left, right, cmpfunc = cmp):
+    for i in range(left, right):
+        k = i
+        for j in range(i+1, right+1):
+            if cmpfunc(L[j], L[k]) < 0:
+                k = j
+        L[i], L[k] = L[k], L[i]
+
+#L = [5,1,3,2,4]
+#print(L)
+#selectsort(L, 0, 4)
+#print(L)
