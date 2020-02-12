88bcc231c662e29efd7a164ad3a6cc3c4f75b4a2
kamilck13 <kamil.ck13@gmail.com>
2020-02-12 20:40:44 +0100

    zmiany
    Zmodyfikowane drukowanie, nazwy funkcji i kilka mniejszych elementów.

--- PROJEKT/BinarySearchTree.py
+++ PROJEKT/BinarySearchTree.py
@@ -12,51 +12,52 @@ class Node:
     def __str__(self):
         return str(self.data)
 
-class Btree:
+class  BinarySearchTree:
     def __init__(self, top = None):     #top - głowa drzewa kalsy node
         self.top = top
 
-    def btree_print_indented(self, top = NOTHING, level = 0):     # funkcja rysująca drzewo
-        if top is NOTHING: top = self.top
-        if top is None:
-            return
-        self.btree_print_indented(top.right, level+1)
-        print ("{}* {}".format('   '*level, top))
-        self.btree_print_indented(top.left, level+1)
-    
-    def _btree_test_print_indented(self, top = NOTHING, level = 0):
+    def print_indented(self, top = NOTHING):     # funkcja wypisujaca elementy drzewa
         if top is NOTHING: top = self.top
         if top is None:
             return ""
-        return str(top.data) + "x" + self._btree_test_print_indented(top.right, level+1) + "x" + self._btree_test_print_indented(top.left, level+1)
+        left = self.print_indented(top.left)
+        right = self.print_indented(top.right)
+        if(left != "" and right != ""):
+            return str(top.data) + "+[" + left + "]+[" + right + "]"
+        elif(left != ""):
+            return str(top.data) + "+[" + left + "]"
+        elif(right != ""):
+            return str(top.data) + "+[" + right + "]"
+        else:
+            return str(top.data)
 
-    def btree_insert(self, node, top = NOTHING):   # wstawia element
+    def insert(self, node, top = NOTHING):   # wstawia element
         if top is NOTHING: top = self.top
         if top is None:
             self.top = node
             return node
         if node.data < top.data:
-            top.left = self.btree_insert(node, top.left)
+            top.left = self.insert(node, top.left)
             top.left.parent = top
         elif node.data > top.data:
-            top.right = self.btree_insert(node, top.right)
+            top.right = self.insert(node, top.right)
             top.right.parent = top
         else:
             pass          # ignorujemy duplikaty
         self.top = top
         return top
 
-    def btree_search(self, data, top = NOTHING):   # zwraca węzeł lub None
+    def search(self, data, top = NOTHING):   # zwraca węzeł lub None
         if top is NOTHING: top = self.top
         if top is None or top.data == data:
             return top
-        node = self.btree_search(data, top.left)
+        node = self.search(data, top.left)
         if node:
             return node
         else:
-            return self.btree_search(data, top.right)  
+            return self.search(data, top.right)  
 
-    def _btree_transplant (self, first, second):
+    def _transplant (self, first, second):
         if first.parent is None:
             self.top = second
             if self.top:
@@ -70,36 +71,36 @@ class Btree:
             second.parent = first.parent
         return
 
-    def _btree_find_min(self, top):
+    def _find_min(self, top):
         if top is None:
             raise ValueError("emnty tree")
         while top.left:
             top = top.left
         return top
 
-    def btree_delete(self, node):         #usuwa element
+    def delete(self, node):         #usuwa element
         if self.top is None or node is None:
             return
         if node.left is None:
-            self._btree_transplant(node, node.right)
+            self._transplant(node, node.right)
             return
         elif node.right is None:
-            self._btree_transplant(node, node.left)
+            self._transplant(node, node.left)
             return
         else:
-            y = self._btree_find_min(node.right)
+            y = self._find_min(node.right)
             if y.parent != node:
-                self._btree_transplant(y, y.right)
+                self._transplant(y, y.right)
                 y.right = node.right
             if y.right:
                 y.right.parent = y
-        self._btree_transplant(node, y)
+        self._transplant(node, y)
         y.left = node.left
         y.left.parent = y
         return
 
     # algorytm DSW -----------------------------------------------------
-    def btree_DSW(self):
+    def DSW(self):
         self._createSpine()
         self._createWeightedTree()        
 
@@ -109,7 +110,7 @@ class Btree:
             return 0
         return 1 + self._spine_height(top.right)
 
-    def _right_rotation(self, root):
+    def _left_rotation(self, root):
         parent = root.parent
         right = root.right
         if right.left != None:
@@ -151,16 +152,16 @@ class Btree:
         #wykonaj n-m rotacji, idąc od początku linii po prawych potomkach
         tmp = self.top
         for _ in range(n-m):
-            tmp = self._right_rotation(tmp)
+            tmp = self._left_rotation(tmp)
             if(tmp.parent == None):
                 self.top = tmp
             tmp = tmp.right
         while m > 1:
-            m = math.floor(m/2)
+            m = m//2
             # wykonaj m rotacji, idąc od początku linii po prawych potomkach
             tmp = self.top
             for _ in range(m):           
-                tmp = self._right_rotation(tmp)
+                tmp = self._left_rotation(tmp)
                 if(tmp.parent == None):
                     self.top = tmp
                 tmp = tmp.right      
@@ -168,43 +169,42 @@ class Btree:
 
 class AllTests(unittest.TestCase):
     def setUp(self):
-        self.root = Btree()         # puste drzewo        
-        self.root.btree_insert(Node(2))
-        self.root.btree_insert(Node(3))
-        self.root.btree_insert(Node(5))
-        self.root.btree_insert(Node(1))
-        self.root.btree_insert(Node(4))
-        self.root.btree_insert(Node(8))
-        self.root.btree_insert(Node(9))
-        self.root.btree_insert(Node(6))
-        self.root.btree_insert(Node(0))
-        self.root.btree_insert(Node(10))
-        self.root.btree_insert(Node(7))
-        #self.root.btree_print_indented()
+        self.root =  BinarySearchTree()         # puste drzewo        
+        self.root.insert(Node(2))
+        self.root.insert(Node(3))
+        self.root.insert(Node(5))
+        self.root.insert(Node(1))
+        self.root.insert(Node(4))
+        self.root.insert(Node(8))
+        self.root.insert(Node(9))
+        self.root.insert(Node(6))
+        self.root.insert(Node(0))
+        self.root.insert(Node(10))
+        self.root.insert(Node(7))
+        #self.root.print_indented()
 
     def test1_solve1(self):
-        wynik = "2x3x5x8x9x10xxxx6x7xxxx4xxxx1xx0xx"
-        self.assertEqual(self.root._btree_test_print_indented(), wynik)
+        wynik = "2+[1+[0]]+[3+[5+[4]+[8+[6+[7]]+[9+[10]]]]]"
+        self.assertEqual(self.root.print_indented(), wynik)
 
     def test2_solve1(self):
-        self.root.btree_DSW()
-        wynik = "7x9x10xxx8xxx3x5x6xxx4xxx1x2xxx0xx"
-        self.assertEqual(self.root._btree_test_print_indented(), wynik)
+        self.root.DSW()
+        wynik = "7+[3+[1+[0]+[2]]+[5+[4]+[6]]]+[9+[8]+[10]]"
+        self.assertEqual(self.root.print_indented(), wynik)
 
     def test3_solve1(self):
-        self.root.btree_delete(self.root.btree_search(7))
-        wynik = "2x3x5x8x9x10xxxx6xxx4xxxx1xx0xx"
-        self.assertEqual(self.root._btree_test_print_indented(), wynik)
+        self.root.delete(self.root.search(7))
+        wynik = "2+[1+[0]]+[3+[5+[4]+[8+[6]+[9+[10]]]]]"
+        self.assertEqual(self.root.print_indented(), wynik)
 
     def test3_solve2(self):
-        self.root.btree_delete(self.root.btree_search(8))
-        wynik = "2x3x5x9x10xxx6x7xxxx4xxxx1xx0xx"
-        self.assertEqual(self.root._btree_test_print_indented(), wynik)
+        self.root.delete(self.root.search(8))
+        wynik = "2+[1+[0]]+[3+[5+[4]+[9+[6+[7]]+[10]]]]"
+        self.assertEqual(self.root.print_indented(), wynik)
 
     def test3_solve3(self):
-        self.root.btree_delete(self.root.btree_search(2))
-        wynik = "3x5x8x9x10xxxx6x7xxxx4xxx1xx0xx"
-        self.assertEqual(self.root._btree_test_print_indented(), wynik)
+        wynik = "2+[1+[0]]+[3+[5+[4]+[8+[6+[7]]+[9+[10]]]]]"
+        self.assertEqual(self.root.print_indented(), wynik)
 
 if __name__ == '__main__':
     unittest.main() 
